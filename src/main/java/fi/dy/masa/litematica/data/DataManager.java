a1136a916cc94c7f841ca80daae0cefbc6c83a65
Sakura Ryoko <116967773+sakura-ryoko@users.noreply.github.com>
2024-05-23 16:12:06 -0400

    Update DataManager.java

--- src/main/java/fi/dy/masa/litematica/data/DataManager.java
+++ src/main/java/fi/dy/masa/litematica/data/DataManager.java
@@ -506,12 +506,5 @@ public class DataManager implements IDirectoryCache
     public static void setToolItem(String itemNameIn)
     {
         toolItem = InventoryUtils.getItemStackFromString(itemNameIn);
-
-        if (toolItem.isEmpty())
-        {
-            // Fall back to a stick
-            toolItem = new ItemStack(Items.STICK);
-            Configs.Generic.TOOL_ITEM.setValueFromString(Registries.ITEM.getId(Items.STICK).toString());
-        }
     }
 }
